152ac3435151e333cd8f35809ce9d415adec9e39
krishna-reddy <krishna.reddy@windstream.com>
2019-02-18 04:13:50 +0000

    updating the smart contract

--- app.js
+++ app.js
@@ -10,18 +10,19 @@ var bodyParser = require('body-parser')
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
-// parse application/json
+// parse an application/json
 app.use(bodyParser.json())
 
 //Create the Ip contract to deploy into ethereum network
 var ipContract = web3.eth.contract([{"constant":true,"inputs":[{"name":"num","type":"uint256"}],"name":"getIpList","outputs":[{"name":"","type":"string"},{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"ipAddress","type":"string"},{"name":"currenttime","type":"uint256"}],"name":"updateIpList","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"ipArray","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"ipList","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"getListLength","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]);
 
+address = '0xf7e4f6c9af4a9b30ffc574e7c438bd8c8aab1c46'
 //Get the all blocked Ips from ethereum network
 app.get('/getBlockedIps', function (req, res) {
   accounts = web3.eth.accounts
   var defaultAccount = web3.eth.accounts[0];
 
-  var contractInstance = ipContract.at('0xFFDAa778B0Df062E843fC35Ef9107730E753b4e7');
+  var contractInstance = ipContract.at(address);
 
   len = contractInstance.getListLength.call();
   var results =  []
@@ -38,7 +39,7 @@ app.post('/blockIp', function (req, res) {
    data = req.body;
    balance = web3.eth.getBalance(web3.eth.accounts[0]);
    console.log(balance)
-   var contractInstance = ipContract.at('0xFFDAa778B0Df062E843fC35Ef9107730E753b4e7');
+   var contractInstance = ipContract.at(address);
    console.log(contractInstance);
    console.log(contractInstance.updateIpList.sendTransaction(data.ip,data.timestamp,{from:web3.eth.accounts[1],gas:3000000}));
    console.log( data );
